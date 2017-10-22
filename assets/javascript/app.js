bbe67d29664f25f8adba14130e47ef20770e3799
Aline Espino <AVE@Alines-MacBook-Air.local>
2017-10-22 16:40:42 -0700

    question loops

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,59 @@
 
 
-// window.onload = function() {
-// run();
-//   // $("#lap").on("click", stopwatch.recordLap);
-//   // $("#stop").on("click", stopwatch.stop);
-//   // $("#reset").on("click", stopwatch.reset);
-//   // $("#start").on("click", stopwatch.start);
-// };
 
-
-
-// variables to store answers : // 
+// variables to store answers // 
 
 var correct = 0;
 var incorrect = 0;
 var noAnswer = 0;
 
 
-
-// timer //
-var timer = 5;
+// timer variables//
+var timer = 60;
 var interval;
-
-
-
-//when counter in "0" switch page and show results // 
-// .on("click",openWin())
 
 
 // FUNCTIONS // 
 
-$(document).ready (function() { 
+// e.preventDefault() // not working //
+
 run();
 
-// start counter when page loads : decrement 1min // 
+//ANSWERS //
+
+var answers = ["b", "a", "b", "c", "c"];
+
+$(document).ready (function(){
+$("#done-btn").on("click",function(e){  
+e.preventDefault();
+
+var userAnswers = [];
+var numQuestions = 5;
+for (var i = 0; i < numQuestions; i++) {
+	var selValue = $('input[name=q'+(i+1)+']:checked').val();
+	userAnswers.push(selValue);
+	console.log(selValue)
+}
+
+
+
+if (answers[1] === userAnswers[1]) {
+	correct ++;
+	$("#correct").text("Correct Ansers: " + " " + correct)
+}
+
+console.log(answers);
+console.log(userAnswers);
+
+})
+
+
+
+// TIMER
 
 function run() {
 interval = setInterval(decrement, 1000);
 }
-
 
 function decrement() {
 timer --;
@@ -48,14 +63,7 @@ if (timer === 0) {
 openWin();
 }
 
-
-
 };
-
-
-// stop counter // 
-// show timer in #counter tag //
-//once hits 0 show results //
 
 function openWin() {
 window.location.replace("results.html"); 
@@ -64,7 +72,6 @@ window.location.replace("results.html");
 
 
 
-// Run once page loads // 
 
 });
 
