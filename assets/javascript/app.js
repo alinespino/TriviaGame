6b3f59e2a1aaed6ef1cb07e67805fc949ffce58e
Aline Espino <AVE@Alines-MacBook-Air.local>
2017-10-22 14:31:16 -0700

    rescued files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,43 @@
 
 
 
+// variables to store answers : // 
+
+var correct = 0;
+var incorrect = 0;
+var noAnswer = 0;
 
 
 
+// timer //
+var timer = 1000;
+
+//when we click on done, stop counter //
+
+("#done-btn").on("click", stop)
+
+//when counter in "0" switch page and show results // 
+
+
+
+// FUNCTIONS // 
+
+
+// start counter when page loads : decrement 1min // 
+
+
+
+// stop counter // 
+
+
+// show timer in #counter tag //
+
+
+//once hits 0 show results //
+
+
+
+// once clicked done 
 
 
 
@@ -17,6 +51,13 @@
 // Run once page loads // 
 
 $(document).ready(function()
+
+
+
+
+
+
+
 
 
 
