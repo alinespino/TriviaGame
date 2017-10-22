46709a177f8da328ffe62c58e5c231ce4cbc3d24
Aline Espino <AVE@Alines-MacBook-Air.local>
2017-10-22 15:42:31 -0700

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
 
 
+// window.onload = function() {
+// run();
+//   // $("#lap").on("click", stopwatch.recordLap);
+//   // $("#stop").on("click", stopwatch.stop);
+//   // $("#reset").on("click", stopwatch.reset);
+//   // $("#start").on("click", stopwatch.start);
+// };
+
+
 
 // variables to store answers : // 
 
@@ -10,56 +19,53 @@ var noAnswer = 0;
 
 
 // timer //
-var timer = 1000;
+var timer = 5;
+var interval;
 
-//when we click on done, stop counter //
 
-("#done-btn").on("click", stop)
 
 //when counter in "0" switch page and show results // 
-
+// .on("click",openWin())
 
 
 // FUNCTIONS // 
 
+$(document).ready (function() { 
+run();
 
 // start counter when page loads : decrement 1min // 
 
+function run() {
+interval = setInterval(decrement, 1000);
+}
+
+
+function decrement() {
+timer --;
+$("#counter").text("Time remaining:" + " " + timer + " " + "secs" );
+
+if (timer === 0) {
+openWin();
+}
+
+
+
+};
 
 
 // stop counter // 
-
-
 // show timer in #counter tag //
-
-
 //once hits 0 show results //
 
-
-
-// once clicked done 
-
-
-
-
-
-
+function openWin() {
+window.location.replace("results.html"); 
+}
 
 
 
 
 // Run once page loads // 
 
-$(document).ready(function()
+});
 
 
-
-
-
-
-
-
-
-
-
-)};
